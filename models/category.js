c33531cb4c328725b2322611d17e12334b5ba809
dlongo <dlongodesign@gmail.com>
2022-04-25 21:21:09 -0400

    add category models and api routes

---
+++ models/category.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    order: Number
+})
+
+export default mongoose.models.Category || mongoose.model("Category", CategorySchema)
---
+++ pages/api/category/[id].js
@@ -0,0 +1,46 @@
+import dbConnect from "../../../util/mongo"
+import Category from "../../../models/category"
+
+export default async function handler(req, res) {
+    const { method, query: { id }, cookies } = req
+    const token = cookies.token
+    await dbConnect()
+
+    if (method === "GET") {
+        try {
+            const categoryItem = await Category.findById(id)
+            res.status(200).json(categoryItem)
+        } catch {
+            console.log("Error getting category data")
+            res.status(500).json(err)
+        }
+    }
+    if (method === "PUT") {
+        if (!token || token !== process.env.TOKEN) {
+            return res.status(401).json("You are not authenticated!")
+        }
+        try {
+            const categoryItem = await Category.findByIdAndUpdate(id, req.body, {
+                new: true,
+            })
+            res.status(200).json(categoryItem)
+
+        } catch (err) {
+            console.log("Error Editing Category Item")
+            res.status(500).json(err)
+        }
+    }
+
+    if (method === "DELETE") {
+        if (!token || token !== process.env.TOKEN) {
+            return res.status(401).json("You are not authenticated!")
+        }
+        try {
+            await Category.findByIdAndDelete(id);
+            res.status(200).json("The Category has been deleted!");
+        } catch {
+            console.log("Error deleting Category Item")
+            res.status(500).json(err)
+        }
+    }
+}
