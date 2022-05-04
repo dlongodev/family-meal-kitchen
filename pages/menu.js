1abcd61f0162eaaad031e29519296a7352f12d30
dlongo <dlongodesign@gmail.com>
2022-05-03 20:45:41 -0400

    add console.log

--- pages/menu.js
+++ pages/menu.js
@@ -45,6 +45,7 @@ export default Menu
 
 export const getServerSideProps = async () => {
     const res = await axios.get(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/menu`)
+    console.log("environment menu page ", process.env.NEXT_PUBLIC_VERCEL_URL)
     return {
         props: {
             menuList: res.data,
