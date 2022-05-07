ade7b2544e948436dd323d6b2ca9350b110c33b9
Daniele Longo <dlongodesign@gmail.com>
2022-05-07 00:13:57 -0400

    Update cart.jsx
    Remove old cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -46,62 +46,6 @@ const Cart = () => {
           us for any special request at <span>754-264-6268</span>
         </Paragraph>
       </Wrapper>
-      <SectionContainer>
-        {checkout ? (
-          <OrderDetail
-            total={cart.total}
-            createOrder={createOrder}
-            setCheckout={setCheckout}
-          />
-        ) : (
-          <FlexDiv flex="column">
-            {cartEmpty ? (
-              <FlexDiv flex="column">
-                <TitleText m="1rem" fw="300">
-                  Your Cart is Empty
-                </TitleText>
-                <Link href="/menu" passHref>
-                  <BtnLinkOutlined m="1rem 0 0 0" w="15rem">
-                    Add Items to Cart
-                  </BtnLinkOutlined>
-                </Link>
-              </FlexDiv>
-            ) : (
-              <FlexDiv flex="column">
-                <TitleText m="1rem" fw="300">
-                  Your Order
-                </TitleText>
-                {cart?.menuItems.map((item, i) => (
-                  <GridTable key={i}>
-                    <div>{item.title}</div>
-                    <Quantity>
-                      <QtyBtn onClick={handleSubtract}>-</QtyBtn>
-                      <span>{item.quantity}</span>
-                      <QtyBtn>+</QtyBtn>
-                    </Quantity>
-                    <div>${item.price * item.quantity}</div>
-                    <ButtonDelete onClick={() => handleDelete(item)}>
-                      delete
-                    </ButtonDelete>
-                  </GridTable>
-                ))}
-                <CartTotalWrapper>
-                  <CartTotalText>Total:</CartTotalText>
-                  <CartTotalText>${cart.total}</CartTotalText>
-                </CartTotalWrapper>
-                <FlexDiv gap="2rem">
-                  <Link href="/menu" passHref>
-                    <BtnLinkOutlined w="15rem">Add More Items</BtnLinkOutlined>
-                  </Link>
-                  <ButtonSolid w="15rem" onClick={() => setCheckout(true)}>
-                    Checkout Now!
-                  </ButtonSolid>
-                </FlexDiv>
-              </FlexDiv>
-            )}
-          </FlexDiv>
-        )}
-      </SectionContainer>
     </>
   );
 };
